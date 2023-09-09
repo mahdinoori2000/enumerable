407a4fab59550577b89881813f7c3571b7b41287
geekelo <efuelight12@gmail.com>
2023-09-09 12:46:25 +0100

    Add and solution

---
+++ my_list.rb
@@ -0,0 +1,16 @@
+require_relative './enumerable'
+class MyList
+  def initialize(*arr)
+    @list = arr
+  end
+  include MyEnum
+  def each(&block)
+    @list.each { |x| block.call(x) }
+  end
+end
+list = MyList.new(1, 2, 3, 4)
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
